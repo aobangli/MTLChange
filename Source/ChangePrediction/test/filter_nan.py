d2ddfe843db2b6a2f16dd3f1902b25f09b07b492
Li Aobang <774164644@qq.com>
2023-06-03 15:42:46 +0800

    处理数据集中的nan值

--- Source/ChangePrediction/test/filter_nan.py
+++ Source/ChangePrediction/test/filter_nan.py
@@ -7,12 +7,11 @@ from Source.ChangePrediction.TrainConfig import data_path, feature_label_list
 df = pd.read_csv(data_path)
 
 # nan_rows = df[df.isna().any(axis=1)]
-#
-# col_list = ['project_avg_rounds_abandoned', 'project_avg_duration_abandoned',
-#                 'project_avg_churn_abandoned', 'project_avg_file_abandoned', 'project_avg_comments_abandoned']
-col_list = feature_label_list
-nan_rows = df[df[col_list].isna().any(axis=1)]
 
+col_list = feature_label_list
+
+# 处理nan值
+nan_rows = df[df[col_list].isna().any(axis=1)]
 
 rows_to_fill = nan_rows.index
 df.loc[rows_to_fill] = df.loc[rows_to_fill].fillna(0)
