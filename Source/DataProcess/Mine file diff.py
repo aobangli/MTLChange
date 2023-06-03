0819614df8dbf04f552affb95df381c4ad792260
Li Aobang <774164644@qq.com>
2023-06-03 14:23:10 +0800

    diff爬取支持所有revision

--- Source/DataProcess/Mine file diff.py
+++ Source/DataProcess/Mine file diff.py
@@ -13,16 +13,17 @@ from Source.DataProcess.DataProcessConfig import *
 
 
 def main():
-    miner = DiffMiner(project, replace=False, verbose=False)
-    change_revision_file_ids_filename = os.path.join(f'{miner.root}/{project}_change_revision_file_ids.csv')
+    # ONLY download files of first revision per change
+    # miner = DiffMiner(project, replace=False, verbose=False)
+    # change_revision_file_ids_filename = os.path.join(f'{miner.root}/{project}_change_revision_file_ids.csv')
     # create_change_summary(miner.root, project, change_revision_file_ids_filename)
+    # miner.mine(change_revision_file_ids_filename)
 
-    # uncomment this if you need to rerun for mining rest of the file diffs.
-    # list_not_mined_selected_changes()
-
-    # only needed for the first time
-    # change_revision_file_ids_filename = os.path.join(f'{miner.root}/{project}_change_revision_file_ids_left.csv')
-    miner.mine(change_revision_file_ids_filename)
+    # download files of all revisions per change
+    miner = DiffMiner(project, diff_path=f"{root}/all_diff", replace=False, verbose=False)
+    change_all_revision_file_ids_filename = os.path.join(f'{miner.root}/{project}_change_all_revision_file_ids.csv')
+    # create_change_all_revision_summary(miner.root, project, change_all_revision_file_ids_filename)
+    miner.mine(change_all_revision_file_ids_filename)
 
 
 def create_change_summary(root, project, change_revision_file_ids_filename):
@@ -56,6 +57,35 @@ def create_change_summary(root, project, change_revision_file_ids_filename):
     output_file.close()
 
 
+def create_change_all_revision_summary(root, project, change_revision_file_ids_filename):
+    change_root = f"{root}/change"
+
+    selected_changes = pd.read_csv(f"{root}/{project}_selected_changes.csv")['change_id'].values
+
+    filenames = [filename for filename in os.listdir(change_root) if is_change_file(filename)]
+    output_file = open(change_revision_file_ids_filename, "w", newline='', encoding='utf-8')
+    writer = csv.writer(output_file, delimiter=',')
+    writer.writerow(['change_number', 'id', 'revision_id', 'file_id'])
+
+    for filename in tqdm(filenames):
+        filepath = os.path.join(change_root, filename)
+        change_jsons = load_change_jsons(open(filepath, "r"))
+
+        for change_json in change_jsons:
+            change = Change(change_json)
+            if change.change_number not in selected_changes:
+                continue
+            for revision in change.revisions:
+                if revision is None:
+                    continue
+                if len(revision.files) > 1000:
+                    print(f'{change.change_number} {revision.number} revision has {len(revision.files)}  files !')
+                    continue
+                for file in revision.files:
+                    writer.writerow([change.change_number, change.id, revision.id, file.path])
+    output_file.close()
+
+
 def is_change_file(filename: str) -> bool:
     status = ["open", "closed", "merged", "abandoned"]
     for s in status:
@@ -71,19 +101,20 @@ def load_change_jsons(input_file):
     return change_json
 
 
-def list_not_mined_selected_changes():
-    filenames = os.listdir(diff_root)
-    changes = [int(filename.split('_')[1]) for filename in filenames]
-    df = pd.read_csv(f'{root}/{project}_selected_changes.csv')
-    df = df[~df['change_id'].isin(changes)]
-    # df.to_csv(f'{root}/{project}_selected_changes_left.csv', index=False)
-
-    changes_left = df['change_id'].values
-
-    df = pd.read_csv(f'{root}/{project}_change_revision_file_ids.csv', encoding='utf-8')
-    df = df[df['change_number'].isin(changes_left)]
-    df.to_csv(f'{root}/{project}_change_revision_file_ids_left.csv', encoding='utf-8', index=False)
-    print(len(changes_left))
+#
+# def list_not_mined_selected_changes():
+#     filenames = os.listdir(diff_root)
+#     changes = [int(filename.split('_')[1]) for filename in filenames]
+#     df = pd.read_csv(f'{root}/{project}_selected_changes.csv')
+#     df = df[~df['change_id'].isin(changes)]
+#     # df.to_csv(f'{root}/{project}_selected_changes_left.csv', index=False)
+#
+#     changes_left = df['change_id'].values
+#
+#     df = pd.read_csv(f'{root}/{project}_change_revision_file_ids.csv', encoding='utf-8')
+#     df = df[df['change_number'].isin(changes_left)]
+#     df.to_csv(f'{root}/{project}_change_revision_file_ids_left.csv', encoding='utf-8', index=False)
+#     print(len(changes_left))
 
 
 class DiffMiner:
@@ -95,11 +126,11 @@ class DiffMiner:
         'opendaylight': "https://git.opendaylight.org/gerrit"
     }
 
-    def __init__(self, project, replace=False, verbose=False):
+    def __init__(self, project, diff_path=None, replace=False, verbose=False):
         self.project = project
         self.replace = replace
         self.root = f"{data_folder}/{project}"
-        self.diff_root = f"{self.root}/diff"
+        self.diff_root = f"{self.root}/diff" if diff_path is None else diff_path
         self.verbose = verbose
 
         try:
@@ -141,8 +172,8 @@ class DiffMiner:
 
         try:
             self.changes[change_number][revision_id][file_id] = json.loads(data)
-        except:
-            print('invalid json format.')
+        except Exception as result:
+            print(f'Error: {result}! Change:{change_number} Revision:{revision_id} File:{file_id} URL:{url}')
             return
 
         self.changes[change_number]['total'] -= 1
@@ -176,14 +207,16 @@ class DiffMiner:
         # change_numbers = change_numbers[:10]
         df = df[df['change_number'].isin(change_numbers)]
 
-        path = f"{self.root}/files_to_download.csv"
+        path = f"{self.root}/{self.project}_files_dict.csv"
         if os.path.exists(path):
             changes = joblib.load(path)
         else:
             for change_number in tqdm(change_numbers):
                 changes[change_number] = {}
                 files = df[df['change_number'] == change_number].reset_index(drop=True)
-                changes[change_number][files.loc[0, 'revision_id']] = {}
+                # changes[change_number][files.loc[0, 'revision_id']] = {}
+                for revision_id in files['revision_id'].unique():
+                    changes[change_number][revision_id] = {}
                 changes[change_number]['total'] = files.shape[0]
             joblib.dump(changes, path)
 
